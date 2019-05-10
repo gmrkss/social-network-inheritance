05e6d88fe481b98cac94954cba46a0156faabad8
gmrkss <parlamasgeorge@gmail.com>
2019-05-10 01:47:02 +0000

    Passes older Python versions

--- social_network/posts.py
+++ social_network/posts.py
@@ -16,7 +16,7 @@ class Post(object):
 
 class TextPost(Post):  # Inherit properly
     def __init__(self, text, timestamp=None):
-        super().__init__(text, timestamp)
+        super(TextPost, self).__init__(text, timestamp)
         
     def __str__(self):
         return '@{user}: "{text}"\n\t{timestamp}'.format(user=self.user,text=self.text,timestamp=self.timestamp.strftime('%A, %b %d, %Y'))
@@ -24,7 +24,7 @@ class TextPost(Post):  # Inherit properly
 
 class PicturePost(Post):  # Inherit properly
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(text, timestamp)
+        super(PicturePost, self).__init__(text, timestamp)
         self.image_url = image_url
 
     def __str__(self):
@@ -33,7 +33,7 @@ class PicturePost(Post):  # Inherit properly
 
 class CheckInPost(Post):  # Inherit properly
     def __init__(self, text, latitude, longitude, timestamp=None):
-        super().__init__(text, timestamp)
+        super(CheckInPost, self).__init__(text, timestamp)
         self.latitude = latitude
         self.longitude = longitude
 
